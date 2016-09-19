ebeb60bf7d6302a7c4e5a48dfa0c47e8b1b73661
阿久津岳志 <sky_10577@yahoo.co.jp>
2016-09-19 19:56:42 +0900

    delete unnecessary files and delete form tag and rocket hash

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   root 'prototypes#index'
 
-  get  '/prototypes/index'  =>    'prototypes#index'
-  get  '/prototypes/new'    =>    'prototypes#new'
-  get  '/prototypes/show'   =>    'prototypes#show'
+  resources :prototypes, only: [:index, :new, :show]
 end
