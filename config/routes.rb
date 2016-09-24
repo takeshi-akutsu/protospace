9411d7291c1096f34f5892cb98ddec28e42a867d
阿久津岳志 <sky_10577@yahoo.co.jp>
2016-09-24 23:15:41 +0900

    fix some bugs

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,5 @@ Rails.application.routes.draw do
   root 'prototypes#index'
 
   resources :prototypes, only: [:index, :new, :show]
-  resources :users, only: [:show, :edit]
+  resources :users, only: [:show, :edit, :update]
 end
