c0c6daab85303f760fc89df888e00ffef45c8059
阿久津岳志 <sky_10577@yahoo.co.jp>
2016-09-24 19:56:17 +0900

    add some function such as sign_in, logout, showing user-page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,21 @@
 class UsersController < ApplicationController
+
+  def index
+  end
+
+  def show
+    @user = current_user
+  end
+
+  def edit
+  end
+
+  def update
+    current_user.update(update_params)
+  end
+
+  private
+  def update_params
+    params.require(:user).permit(:nickname, :email, :password, :member, :self_intro, :works)
+  end
 end
