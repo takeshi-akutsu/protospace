e7ebc13ac4ef9d02374e9807e46fd50ad1af8388
阿久津岳志 <sky_10577@yahoo.co.jp>
2016-09-26 17:25:50 +0900

    Add prototype and user tables and models

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160916084438) do
+ActiveRecord::Schema.define(version: 20160926082135) do
+
+  create_table "images", force: :cascade do |t|
+    t.string   "image",      limit: 255
+    t.integer  "status",     limit: 4
+    t.integer  "project_id", limit: 4
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
+  end
+
+  create_table "prototypes", force: :cascade do |t|
+    t.text     "title",      limit: 65535
+    t.text     "catch_copy", limit: 65535
+    t.text     "concept",    limit: 65535
+    t.integer  "user_id",    limit: 4
+    t.datetime "created_at",               null: false
+    t.datetime "updated_at",               null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  limit: 255,   default: "", null: false
