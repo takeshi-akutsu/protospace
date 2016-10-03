212b6d0ee4d411694d7fc1680801681504b41975
阿久津岳志 <sky_10577@yahoo.co.jp>
2016-10-03 12:11:15 +0900

    change self_defined_method to enum

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,4 +1,6 @@
 class Image < ActiveRecord::Base
   belongs_to :prototype
   mount_uploader :image, ImageUploader
+
+  enum status: { main: 1, sub: 2 }
 end
--- app/models/prototype.rb
+++ app/models/prototype.rb
@@ -2,22 +2,4 @@ class Prototype < ActiveRecord::Base
   has_many :images
   belongs_to :user
   accepts_nested_attributes_for :images
-
-  def main_image
-    self.images.each do |image|
-      if image.status == 1
-        return image
-      end
-    end
-  end
-
-  def sub_images
-    images = []
-    self.images.each do |image|
-      if image.status == 2
-        images << image
-      end
-    end
-    images
-  end
 end
