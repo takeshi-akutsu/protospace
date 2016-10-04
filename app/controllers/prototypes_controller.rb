9d962a25f03243cf1251c8863a480f2128b4b4c2
阿久津岳志 <sky_10577@yahoo.co.jp>
2016-10-04 13:09:41 +0900

    delete comment-outed code

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,7 +1,7 @@
 class PrototypesController < ApplicationController
 
   before_action :authenticate_user!, only: [:new] #まだ適当
-  before_action :get_prototype, only: [:show]
+  before_action :set_prototype, only: [:show]
 
   def index
     @prototypes = Prototype.includes([:images, :user]).page(params[:page]).per(8)
@@ -17,15 +17,12 @@ class PrototypesController < ApplicationController
 
   def create
     current_user.prototypes.create(prototype_params)
-    # prototype.images.each do |image|
-    #   Image.create(image: image.image, status: image.status, prototype_id: image.prototype_id)
-    # end
     redirect_to root_path
   end
 
   private
 
-  def get_prototype
+  def set_prototype
     @prototype = Prototype.find(params[:id])
   end
 
