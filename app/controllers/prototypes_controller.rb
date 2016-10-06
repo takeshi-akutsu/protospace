9b0fe154575a7826fe824b988ca6ad21165ba09c
阿久津岳志 <sky_10577@yahoo.co.jp>
2016-10-06 23:51:30 +0900

    add prototype update function

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,7 +1,7 @@
 class PrototypesController < ApplicationController
 
   before_action :authenticate_user!, only: [:new] #まだ適当
-  before_action :set_prototype, only: [:show]
+  before_action :set_prototype, only: [:show, :edit, :update]
 
   def index
     @prototypes = Prototype.includes([:images, :user]).page(params[:page]).per(8)
@@ -20,6 +20,16 @@ class PrototypesController < ApplicationController
     redirect_to root_path
   end
 
+  def edit
+  end
+
+  def update
+    binding.pry
+    prototype = Prototype.find(params[:id])
+    prototype.update(prototype_params) if (user_signed_in?) && (current_user.id == @prototype.user.id)
+    redirect_to action: :show
+  end
+
   private
 
   def set_prototype
@@ -27,6 +37,6 @@ class PrototypesController < ApplicationController
   end
 
   def prototype_params
-    params.require(:prototype).permit(:title, :catch_copy, :concept, images_attributes: [:image, :status])
+    params.require(:prototype).permit(:title, :catch_copy, :concept, images_attributes: [:id, :image, :status])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users #ログイン周りのルーティングを生成するヘルパーメソッド
   root 'prototypes#index'
-  resources :prototypes, except: [:edit, :update, :destroy]
+  resources :prototypes, except: [:destroy]
   resources :users, only: [:show, :edit, :update]
 end
