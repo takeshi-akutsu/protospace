262ad6d76f27d0b34cfbdee5ab46fc7abf338d47
阿久津岳志 <sky_10577@yahoo.co.jp>
2016-10-19 17:15:31 +0900

    fix issue

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users #ログイン周りのルーティングを生成するヘルパーメソッド
   root 'prototypes#index'
 
-  resources :prototypes, except: [:edit, :update, :destroy] do
+  resources :prototypes do
     resources :likes, only: [:create, :destroy]
   end
   resources :users, only: [:show, :edit, :update]
