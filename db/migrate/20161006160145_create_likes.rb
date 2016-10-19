11234d824ec4a26e159d0f14f0f59742428c4d03
阿久津岳志 <sky_10577@yahoo.co.jp>
2016-10-19 15:28:04 +0900

    refactoring

--- db/migrate/20161006160145_create_likes.rb
+++ db/migrate/20161006160145_create_likes.rb
@@ -1,8 +1,8 @@
 class CreateLikes < ActiveRecord::Migration
   def change
     create_table :likes do |t|
-      t.integer :user_id
-      t.integer :prototype_id
+      t.integer :user_id, null: false
+      t.integer :prototype_id, null: false
       t.timestamps null: false
     end
   end
