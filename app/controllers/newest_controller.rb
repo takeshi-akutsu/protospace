d21fbad743581f09397d972bf76caad8513312b7
阿久津岳志 <sky_10577@yahoo.co.jp>
2016-10-26 11:57:01 +0900

    refactoring

--- app/controllers/newest_controller.rb
+++
@@ -1,5 +0,0 @@
-class NewestController < ApplicationController
-  def index
-    @prototypes = Prototype.includes([:images, :user]).order("created_at DESC").page(params[:page]).per(8)
-  end
-end
--- app/controllers/popular_controller.rb
+++ app/controllers/popular_controller.rb
@@ -1,5 +1,5 @@
 class PopularController < ApplicationController
   def index
-    @prototypes = Prototype.includes([:images, :user]).order("likes_count DESC").limit(4).page(params[:page]).per(8)
+    @prototypes = Prototype.includes([:images, :user]).order_popular.limit(4).page(params[:page]).per(8)
   end
 end
