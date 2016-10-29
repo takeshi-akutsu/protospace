3e92a7aef44c286d47711cff4c561cdae54b0a23
阿久津岳志 <sky_10577@yahoo.co.jp>
2016-10-30 03:29:46 +0900

    add_card_function in prototype_new_page

--- app/assets/javascripts/new_form.js
+++ app/assets/javascripts/new_form.js
@@ -1,6 +1,15 @@
 $(document).on('turbolinks:load',function(){
-  var num = 0;
+  // プロトタイプ作成画面の方
   $("#add_new_form").on("click", function(){
+    console.log("hoge");
+    $("#js-show-form").removeClass("hide-form")
+    $("#js-show-form").addClass("appear-form")
+    $("#add_new_form").addClass("hide");
+  });
+
+  //プロトタイプ編集画面の方
+  var num = 0;
+  $("#add_new_forms").on("click", function(){
     console.log("hello");
     $(this).data("click", ++num);
     var click = $(this).data("click");
@@ -8,7 +17,7 @@ $(document).on('turbolinks:load',function(){
     $(".js-show-sub-" + click + "-image-form").addClass("appear-form")
 
     if (click == 3 ) {
-      $("#add_new_form").addClass("hide");
+      $("#add_new_forms").addClass("hide");
     }
   });
 });
